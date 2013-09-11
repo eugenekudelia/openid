f50c060343a470e7eda11bf63920a3f86b7e6067
Eugene Kudelia <eugene.kudelia@gmail.com>
2013-09-12 03:38:32 +0400

    Config (enabled providers) added

---
+++ config/openid.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * OpenID providers.
+ */
+return array(
+	'enabled' => array(
+		'google'		=> 'Google',
+		'livejournal'	=> 'Livejournal',
+		'myopenid'		=> 'MyOpenID',
+		'wordpress'		=> 'Wordpress',
+		'Yahoo'			=> 'Yahoo',
+	)
+);
