816249b2661adb477b782411235c07a7f96a8d11
Eugene Kudelia <eugene.kudelia@gmail.com>
2013-09-12 15:10:36 +0400

    Config updated

--- config/openid.php
+++ config/openid.php
@@ -2,12 +2,43 @@
 /**
  * OpenID providers.
  */
+//$route = Route::get(APPALIAS.'sso.auth');
+
 return array(
-	'enabled' => array(
-		'google'		=> 'Google',
-		'livejournal'	=> 'Livejournal',
-		'myopenid'		=> 'MyOpenID',
-		'wordpress'		=> 'Wordpress',
-		'yahoo'			=> 'Yahoo',
-	)
+	//'enabled' => array(
+		//'openid'      => array(
+		//	'icon' => '',
+		//	'title' => 'OpenID',
+		//	'id'    => cmn__('Enter full :service identifier', array(':service' => 'OpenID')),
+		//	'uri'   => $route->uri(array('directory' => 'openid', 'controller' => 'openid', 'action' => 'login')),
+		//),
+		//'wordpress'   => array(
+		//	'icon' => '',
+		//	'title' => 'Wordpress',
+		//	'id' => cmn__('Enter your :service username', array(':service' => 'WP')),
+		//	'uri'   => $route->uri(array('directory' => 'openid', 'controller' => 'wordpress', 'action' => 'login')),
+		//),
+		//'yahoo'       => array(
+		//	'icon' => '',
+		//	'title' => 'Yahoo!',
+		//	'uri'   => $route->uri(array('directory' => 'openid', 'controller' => 'yahoo', 'action' => 'login')),
+		//),
+		//'myopenid'    => array(
+		//	'icon' => '',
+		//	'title' => 'MyOpenID',
+		//	'id' => cmn__('Enter your :service username', array(':service' => 'MyOpenID')),
+		//	'uri'   => $route->uri(array('directory' => 'openid', 'controller' => 'myopenid', 'action' => 'login')),
+		//),
+		//'google'      => array(
+		//	'icon' => '',
+		//	'title' => 'Google',
+		//	'uri'   => $route->uri(array('directory' => 'openid', 'controller' => 'google', 'action' => 'login')),
+		//),
+		//'livejournal' => array(
+		//	'icon' => '',
+		//	'title' => 'LiveJournal',
+		//	'id' => cmn__('Enter your :service username', array(':service' => 'LJ')),
+		//	'uri'   => $route->uri(array('directory' => 'openid', 'controller' => 'livejournal', 'action' => 'login')),
+		//),
+	//),
 );
